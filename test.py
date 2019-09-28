f8680969096c9aff0e57695bc4e30946b2e064e5
Lukas Arlauskas <arlauskaslukas@gmail.com>
2019-09-28 20:23:32 +0300

    finished functions

--- test.py
+++ test.py
@@ -2,19 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import pprint
 
 def skaitymas():
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('herithon_secret.json', scope)
     client = gspread.authorize(creds)
     sheet = client.open('photo data herithon').sheet1
-    data = sheet.get_all_records()
-    print(data)
+    X = [ int(x.value) for x in sheet.range('D4:D100')] + [int(x.value) for x in sheet.range('K4:K100')] + [ int(x.value) for x in sheet.range('R4:R100')]
+    Y = [ int(x.value) for x in sheet.range('E4:E100')] + [ int(x.value) for x in sheet.range('L4:L100')] + [ int(x.value) for x in sheet.range('S4:S100')]
+    #igX = [ x.value for x in sheet.range('D4:D100')]
+    #igY = [ x.value for x in sheet.range('E4:E100')]
+    #bX = [ x.value for x in sheet.range('K4:K100')]
+    #fbY = [ x.value for x in sheet.range('L4:L100')]
+    #twX = [ x.value for x in sheet.range('R4:R100')]
+    #twY = [ x.value for x in sheet.range('S4:S100')]
+    #X = igX + fbX + twX
+    #Y = igY + fbY + twY
+    return X, Y
 
 
 def plotas(xcord, ycord, count):
-    colors = np.random.rand()
-    pl = plt.scatter(x,y,s=count,c=colors,alpha=0.7)
+    color = np.random.rand(count)
+    pl = plt.scatter(xcord,ycord,s=20,c=color,alpha=0.7)
+    plt.xticks(np.arange(0, 101, 20).sort())
+    plt.yticks(np.arange(0, 101, 20).sort())
+    plt.title('The graph for scatter map')
     return pl
 
-skaitymas()
+x, y = skaitymas()
+plot = plotas(x,y, 97*3)
+plt.show()
