5fa762d2dee6ac5cfe3abbcb471727ec075d5b85
Lukas Arlauskas <arlauskaslukas@gmail.com>
2019-09-29 10:12:57 +0300

    added kmeans

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import random
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pprint
+from sklearn.cluster import KMeans
+from pandas import DataFrame
 
 def skaitymas():
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
@@ -17,10 +19,21 @@ def skaitymas():
 
 def plotas(xcord, ycord, count):
     color = np.random.choice(500, size=count)
-    pl = plt.scatter(xcord,ycord,s=color,alpha=0.7)
+    pl = plt.scatter(xcord,ycord,c=color,alpha=0.7)
     plt.title('The graph for scatter map')
+    plt.ylabel('Y coord')
+    plt.xlabel('X coord')
     return pl
+
+def kMeansClustering(clusterCount, X, Y, plotting):
+    Data = {'x': X, 'y': Y}
+    df = DataFrame(Data, columns=['x','y'])
+    kMeans = KMeans(n_clusters=clusterCount).fit(df)
+    centroids = kMeans.cluster_centers_
+    plotting = plt.scatter(centroids[:, 0], centroids[:,1], c='red', s=50)
+    return plotting
 
 x, y = skaitymas()
 plot = plotas(x,y, 97*3)
+plotas = kMeansClustering(5, x,y, plot)
 plt.show()
